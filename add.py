e4ee5c4f9fe6a07b2ff2972827a738b84bd54b1a
Vladislav <vlad.kharchenko.96@list.ru>
2024-10-01 14:17:04 +0300

    finish independent

--- add.py
+++ add.py
@@ -1,2 +1,9 @@
-def hello():
-    print("Hello!")
+import math
+
+def calculate_area(a, b, c):
+    if a + b <= c or a + c <= b or b + c <= a:
+        raise ValueError("Такой треугольник не существует.")
+
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    return area
--- run.py
+++ run.py
@@ -1,17 +1,16 @@
-global result
-def rectangle():
-    a = float(input("Ширина: "))
-    b = float(input("Высота: "))
-    global result
-    result = a*b
-def triangle():
-    a = float(input("Основание: "))
-    h = float(input("Высота: "))
-    global result
-    result = 0.5*a*h
-figure = input("1 - Прямоугольник, 2 - треугольник:")
-if figure == '1':
-    rectangle()
-elif figure == '2':
-    triangle()
-print(f"Площадь: {result}")
+from add import calculate_area
+
+def main():
+    try:
+        a = float(input("Введите длину первой стороны треугольника: "))
+        b = float(input("Введите длину второй стороны треугольника: "))
+        c = float(input("Введите длину третьей стороны треугольника: "))
+
+        area = calculate_area(a, b, c)
+        print("Площадь треугольника:", area)
+
+    except ValueError as e:
+        print("Ошибка:", e)
+
+if __name__ == "__main__":
+    main()
